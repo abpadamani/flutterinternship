8ea4840ad935e49f9847560693d50b6a8f8cff5a
abpadamani <arpanpadamani2000@gmail.com>
2022-06-02 12:11:27 +0530

    snackbar completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:internship/snackbar/snackbar.dart';
 
-class  MyApp extends StatelessWidget {
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Arpan"),
-      ),
+    return MaterialApp(
+      home: snackbar(),
     );
   }
 }
