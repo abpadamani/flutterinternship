c9bf104c5164f904a6a307fa6052a7611fc5ba58
abpadamani <arpanpadamani2000@gmail.com>
2022-06-03 17:24:03 +0530

    scrolllist complete

---
+++ lib/ScrollList/slist.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
+import 'package:internship/main.dart';
+
+class Mylist extends StatefulWidget {
+  const Mylist({Key? key}) : super(key: key);
+
+  @override
+  State<Mylist> createState() => _MylistState();
+}
+
+class _MylistState extends State<Mylist> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Contacts")),
+      body: SafeArea(
+          child: Container(
+        child: ListView(
+          children: [
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Text("A"),
+              ),
+              title: Text("Arpan"),
+              subtitle: Text("+919662511313"),
+              trailing: Icon(CupertinoIcons.phone),
+            ),
+          ],
+        ),
+      )),
+    );
+  }
+}
