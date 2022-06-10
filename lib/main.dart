ab632665b2a1b4d65ec692a5b54fb5a22529d74a
abpadamani <arpanpadamani2000@gmail.com>
2022-06-10 18:49:45 +0530

    handle changes textfield completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,69 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:internship/alertdialogue/alert.dart';
-import 'package:internship/mediaquery/media.dart';
 
-import 'package:internship/singlechildscrollview/singlechild.dart';
-import 'package:internship/streamBuilder/Mystream.dart';
-
-import 'Inkwell/loginpage.dart';
-import 'backpressedmanaged/back.dart';
-import 'data-passing/screentwo.dart';
-import 'loginscreen/login.dart';
-import 'orientation/oriantation.dart';
-import 'resUI/screens/home/home.dart';
-
-void main(List<String> args) {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-      theme: ThemeData(
-          textTheme: GoogleFonts.ubuntuTextTheme(
-        Theme.of(context).textTheme,
-      )),
-      // initialRoute: 'login',
-      // routes: {
-      //   'login': (context) => MyLogin(),
-      // },
+    return const MaterialApp(
+      title: 'Retrieve Text Input',
+      home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  void _printLatestValue() {
+    print('Second text field: ${myController.text}');
+  }
+
+  String? response;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Retrieve Text Input'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              onChanged: (text) {
+                response = text;
+              },
+            ),
+            TextField(
+              controller: myController,
+            ),
+            Text('$response'),
+          ],
+        ),
+      ),
     );
   }
 }
